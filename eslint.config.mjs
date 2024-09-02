cc40a0517f62f467278dadbeb5fc052e5e047762
david <davidrichakerja@gmail.com>
2024-09-02 23:12:12 +0700

    started eslint usage

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,12 +6,11 @@ export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
-  { 
-      rules: {
-        "no-var": "warn",
-        "no-unreachable": "warn",
-        "no-duplicate-imports": "warn",
-      }
-
+  {
+    rules: {
+      "no-var": "warn",
+      "no-unreachable": "warn",
+      "no-duplicate-imports": "warn",
+    }
   }
 ];
