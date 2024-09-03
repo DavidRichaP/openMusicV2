8c325f28ffbc1da5b3e1d93a5971f2f3cd1ad03d
david <davidrichakerja@gmail.com>
2024-09-03 21:04:01 +0700

    added authentication

--- src/api/albums/routes.js
+++ src/api/albums/routes.js
@@ -5,26 +5,41 @@ const AlbumRoutes = (handler) => [
         method: 'POST',
         path: '/albums',
         handler: handler.postAlbumHandler,
+        options: {
+            auth: 'openmusic_jwt',
+        },
     },
     {
         method: 'GET',
         path: '/albums',
         handler: handler.getAlbumsHandler,
+        options: {
+            auth: 'openmusic_jwt',
+        },
     },
     {
         method: 'GET',
         path: '/albums/{id}',
         handler: handler.getAlbumByIdHandler,
+        options: {
+            auth: 'openmusic_jwt',
+        },
     },
     {
         method: 'PUT',
         path: '/albums/{id}',
         handler: handler.putAlbumByIdHandler,
+        options: {
+            auth: 'openmusic_jwt',
+        },
     },
     {
         method: 'DELETE',
         path: '/albums/{id}',
         handler: handler.deleteAlbumByIdHandler,
+        options: {
+            auth: 'openmusic_jwt',
+        },
     },
 ];
 
--- src/api/songs/routes.js
+++ src/api/songs/routes.js
@@ -4,26 +4,39 @@ const SongRoutes = (handler) => [
         method: 'POST',
         path: '/songs',
         handler: handler.postSongHandler,
+        options: {
+            auth: 'openmusic_jwt',
+        },
     },
     {
         method: 'GET',
         path: '/songs',
         handler: handler.getSongsHandler,
+        options: {
+            auth: 'openmusic_jwt',
+        },
     },
     {
         method: 'GET',
         path: '/songs/{id}',
         handler: handler.getSongByIdHandler,
+        options: {
+            auth: 'openmusic_jwt',
+        },
     },
     {
         method: 'PUT',
         path: '/songs/{id}',
         handler: handler.putSongByIdHandler,
+        options:
     },
     {
         method: 'DELETE',
         path: '/songs/{id}',
         handler: handler.deleteSongByIdHandler,
+        options: {
+            auth: 'openmusic_jwt',
+        },
     },
 ];
 
