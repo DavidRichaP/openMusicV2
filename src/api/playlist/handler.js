dd49986a40bd98e8e01654714c58bffe05e17927
david <davidrichakerja@gmail.com>
2024-09-11 01:15:07 +0700

    added playlist content management features

--- src/api/playlist/handler.js
+++ src/api/playlist/handler.js
@@ -3,17 +3,23 @@ const autoBind = require("auto-bind")
 class playlistHandler{
   constructor(service, validator) {
     this._service = service.playlist
+    this._usersService = service.user
+    this._songsService = service.song
     this._validator = validator.playlist
+    this._validatorPlaylistSong = validator.playlistSong
 
     autoBind(this)
   }
 
   async postPlaylistHandler(request, h) {
+    const { id: credentialId } = request.auth.credentials;
+
+    const idUser = await this._usersService.verifyUserExist(credentialId);
+    request.payload.owner = idUser;
+
     this._validator.validatePlaylistPayload(request.payload)
 
-    const { name } = request.payload
-
-    const playlist = await this._service.addPlaylist({ name })
+    const playlist = await this._service.addPlaylist(request.payload)
 
     const response = h.response({
       status: 'success',
@@ -25,34 +31,95 @@ class playlistHandler{
     response.code(201)
     return response
   }
-
-  async getPlaylistsHandler(request, h) {
-
-  }
   
-  async getPlaylistByIdHandler(request, h) {
-    const { id } = request.params
-    const { id: credentialId } = request.auth.credentials
+  async getPlaylistByIdHandler(request) {
+    const { id: userId } = request.auth.credentials;
+    const playlists = await this._service.getPlaylist(userId);
 
-    await this._service.verifyPlaylistAccess(id, credentialId)
-    const playlist = await this._service // getplaylist
+
+    //await this._service.verifyPlaylistAccess(id, credentialId)
+    //const playlist = await this._service.getPlaylist()
     
     return {
       status: 'success',
       data: {
-        playlist,
+        playlists,
       },
     }
   }
 
   async deletePlaylistByIdHandler(request, h) {
+    const { id: credentialId } = request.auth.credentials;
+    const { id: playlistId } = request.params;
 
-    const { id } = request.params
-    await this._service.deletePlaylist(id)
+    await this._service.verifyPlaylistOwner(credentialId, playlistId);
+
+    await this._service.deletePlaylist(playlistId);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Playlist berhasil dihapus',
+    });
+
+    return response;
+  }
+
+  async postPlaylistSongByIdHandler(request, h) {
+    const { id: credentialId } = request.auth.credentials;
+    const { id: playlistId } = request.params;
+    const { songId } = request.payload;
+
+    request.payload.playlistId = playlistId;
+    await this._validatorPlaylistSong.validatePlaylistSongPayload(request.payload);
+    await this._songsService.verifySongExist(songId);
+    await this._service.verifyPlaylistAccess(credentialId, playlistId);
+
+    const playlistSong = await this._service.addSongToPlaylist(request.payload, credentialId);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Playlist Song berhasil ditambahkan',
+      data: {
+        playlistSong,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async getPlaylistSongsByIdHandler(request) {
+    const { id: credentialId } = request.auth.credentials;
+    const { id: playlistId } = request.params;
+
+    await this._service.verifyPlaylistAccess(credentialId, playlistId);
+
+    const playlist = await this._service.getPlaylistById(credentialId, playlistId);
+    const songs = await this._service.getPlaylistSongs(playlistId);
+    playlist.songs = songs;
+
     return {
       status: 'success',
-      message: 'Playlist berhasil dihapus'
-    }
+      data: {
+        playlist,
+      },
+    };
+  }
+
+  async deletePlaylistSongByIdHandler(request, h) {
+    const { id: credentialId } = request.auth.credentials;
+    const { id: playlistId } = request.params;
+    const { songId } = request.payload;
+
+    await this._service.verifyPlaylistAccess(credentialId, playlistId);
+
+    await this._service.deletePlaylistSong(playlistId, songId, credentialId);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Playlist Songs berhasil dihapus',
+    });
+
+    return response;
   }
 
 }
